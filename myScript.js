eca99457637970c73e67a9579d38ea82b32f1385
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-03-14 23:11:44 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $("button").click(function(){
-    $("p").toggle();
+    $("#p1").fadeIn();
+   $("#p2").fadeIn();
+   $("#p3").fadeIn();
+   $("#p4").fadeIn();
+   $("#p5").fadeIn();
+   $("#p6").fadeIn();
+    
   });
 });
